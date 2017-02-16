919340ac1e01ba2e5bb454c81306da9fc568a06f
goncalo5 <goncalo.s.and@gmail.com>
2017-02-16 18:05:34 +0000

    now we can write

--- presentation.py
+++ presentation.py
@@ -13,7 +13,33 @@ class Menu(object):
         #self.root.focus_set()
         #self.root.configure(background='gray')
 
-        self.editor = Label(background=BACKGROUND, font=FONT, padx=1000, pady=1000)
+        self.canvas = Canvas(self.root, width=50, height=200)
+        self.canvas.focus_force()
+        self.canvas.grid(columnspan=4)
+        self.canvas.bind('<Key>', self.write)
+
+        self.editor = Label(self.root, text='sad',
+                            background=BACKGROUND, fg='white',
+                            font=FONT, padx=100, pady=100)
         self.editor.grid()
 
         self.root.mainloop()
+
+
+    def write(self, event):
+        op = {'plus': '+', 'minus': '-', 'asterisk': '*', 'slash': '/', 'period': '.'}
+        char = event.keysym
+        print char
+        if char == '??':
+            return
+        elif char == 'Return':
+            self.equal()
+        elif char == 'BackSpace':
+            self.ac()
+        else:
+            if char in op:
+                char = op[char]
+            self.print_value(char)
+
+    def print_value(self, character):
+        self.editor['text'] += character
