71a7fc07b35ac068017b1a8a9991587bc718e701
goncalo5 <goncalo.s.and@gmail.com>
2017-02-16 17:23:29 +0000

    update tkinter in presentation

---
+++ presentation.py
@@ -0,0 +1,10 @@
+from Tkinter import *
+from constants import *
+
+
+class Menu(object):
+    def __init__(self):
+        self.root = Tk()
+        self.root.title('Calculator')
+
+        self.root.mainloop()
---
+++ script.py
@@ -0,0 +1,4 @@
+from presentation import Menu
+
+if __name__ == '__main__':
+    Menu()
